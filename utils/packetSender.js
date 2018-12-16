fe6fed1b00bc8ec699e58ad8dea84189a63c4973
rglr <spore_09@mail.ru>
2018-12-16 13:33:37 +0900

    Fix: missing comma in packetSender

--- utils/packetSender.js
+++ utils/packetSender.js
@@ -3,6 +3,7 @@ const { f2b } = require('../utils/float2bytes');
 
 module.exports = function packetSender(socket, cmd, floats) {
     socket.write(cmd);
+    socket.write(',');
     socket.write(f2b(floats));
     socket.write(',\n');
 };
