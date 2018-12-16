8e3ef7abf1bd66787a769921a03bd6a6b7984445
rglr <spore_09@mail.ru>
2018-12-16 16:43:15 +0900

    Improve: client_single

--- bin/client_single.js
+++ bin/client_single.js
@@ -1,19 +1,53 @@
 const net = require('net');
+const packetExecute = require('../parallels/packetExecute');
+const PacketFinder = require('../utils/PacketFinder');
+const packetSender = require('../utils/packetSender');
+
+
+function info(message) {
+    console.info(`${process.pid}: ${message}`);
+}
+
+function error(message) {
+    console.error(`${process.pid}: ${message}`);
+}
 
 function setupConnection(address, port, retryTimeout = 1000) {
     const client = new net.Socket();
 
-    client.on('error', (error) => {
-        console.error('connection error', error);
+    client.on('error', () => {
+        error('connection error');
     });
 
     client.on('close', () => {
-        console.info('connection closed');
+        info('connection closed');
         setTimeout(() => setupConnection(address, port, retryTimeout), retryTimeout);
     });
 
     client.connect(port, address, () => {
-        console.info(`connection established to ${address}:${port}`);
+        info(`connection established to ${address}:${port}`);
+        info('wait for task...');
+    });
+
+    const finder = new PacketFinder();
+    client.on('data', (buffer) => {
+        finder.addData(buffer);
+
+        let data = finder.getAndRemove();
+        while (data) {
+            info(`execute new task: ${data.message}/${data.data.length}`);
+            try {
+                const result = packetExecute(data.message, data.data);
+
+                info(`task ${data.message} success: ${result}`);
+                packetSender(client, 'ok', [result]);
+            } catch (e) {
+                error(e);
+                packetSender(client, 'error', [1]);
+            }
+
+            data = finder.getAndRemove();
+        }
     });
 }
 
