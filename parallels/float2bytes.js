4a271d2119ff4be35fc16699542c2830515457e3
rglr <spore_09@mail.ru>
2018-12-16 10:32:38 +0900

    Add: float2bytes util

---
+++ parallels/float2bytes.js
@@ -0,0 +1,9 @@
+module.exports = {
+    f2b(floatArray) {
+        return Buffer.from(new Float64Array(floatArray).buffer);
+    },
+    b2f(byteArray) {
+        const uint = new Uint8Array(Buffer.from(byteArray));
+        return new Float64Array(uint.buffer, 0, uint.buffer.length);
+    },
+};
---
+++ tests/float2bytes.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { f2b, b2f } = require('../parallels/float2bytes');
+
+
+describe('float2bytes', () => {
+    const original = [3.5, 0, 5, 10000, 0xffffff, 0, 0, 1, -1];
+
+    it('f2b correct length', () => {
+        assert.equal(f2b(original).length, original.length * 8);
+    });
+
+    it('b2f correct data', () => {
+        const bytes = f2b(original);
+        const originalFromBytes = b2f(bytes);
+
+        assert.deepEqual(originalFromBytes, original);
+    });
+});
