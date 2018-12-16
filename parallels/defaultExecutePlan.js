acd77bf09c0d2616622731b63bfe98f729a143f4
rglr <spore_09@mail.ru>
2018-12-16 13:24:38 +0900

    Improve: full default execute plan

--- parallels/defaultExecutePlan.js
+++ parallels/defaultExecutePlan.js
@@ -1,20 +1,25 @@
 const {
-    prepareDataRaw,
     createExecutePlanChunked,
 } = require('../math');
 
-module.exports = function getPlan(dataRaw, chunkLimit=10) {
+module.exports = function getPlan(dataRaw, chunkLimit = 10) {
     const dataRawPartLength = dataRaw.length / 10; // FIXME
 
-    return [{
+    return createExecutePlanChunked([{
         name: 'm1',
-        dataRaw: dataRaw,
-        chunkLimit
+        dataRaw: dataRaw.slice(0, dataRawPartLength),
+        chunkLimit,
     }, {
-
+        name: 'm2',
+        dataRaw: dataRaw.slice(0, dataRawPartLength),
+        chunkLimit,
     }, {
-
+        name: 'm3',
+        dataRaw,
+        chunkLimit,
     }, {
-
-    }]
+        name: 'm4',
+        dataRaw: dataRaw.slice(0, dataRawPartLength),
+        chunkLimit,
+    }]);
 };
