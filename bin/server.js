954c7f69641b64bdfd026fca5eb25a8e051b695e
rglr <spore_09@mail.ru>
2018-12-16 16:43:46 +0900

    Improve: server; Add: queueWorker

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,10 @@
 const net = require('net');
 const _ = require('lodash');
 const { Spinner } = require('clui');
+const Task = require('../parallels/Task');
+const createQueueWorker = require('../parallels/queueWorker');
+const getPlan = require('../parallels/defaultExecutePlan');
+const { prepareDataRaw } = require('../math');
 
 const clients = [];
 
@@ -8,9 +12,6 @@ const countdown = new Spinner('waiting for clients...', ['|', '/', '—', '\\'])
 countdown.update = () => {
     countdown.message(`clients count: ${clients.length}       `);
 };
-
-const address = process.env.PARALLEL_ADDRESS || '0.0.0.0';
-const port = parseInt(process.env.PARALLEL_PORT, 10) || 9000;
 
 const server = net.createServer((socket) => {
     clients.push(socket);
@@ -24,6 +25,7 @@ const server = net.createServer((socket) => {
 
 process.stdin.once('data', () => {
     countdown.stop();
+    server.close();
 
     if (!clients.length) {
         process.exit(1);
@@ -36,8 +38,31 @@ process.stdin.once('data', () => {
     clients.forEach((socket, index) => {
         console.debug(`[*] ${index + 1}: ${socket.remoteAddress}`);
     });
+
+    const startMs = Date.now();
+    const queue = createQueueWorker(clients);
+
+    const plan = getPlan(prepareDataRaw(10 ** 5), 5000);
+    plan.forEach(p => queue.push(p));
+
+    queue.on('task_finish', (taskId, result) => {
+        console.log('TASK FINISH!', taskId, result);
+        console.log(queue.length);
+    });
+
+    queue.on('task_failed', (taskId, error) => {
+        console.log('TASK failed!', taskId, error);
+    });
+
+    queue.on('empty', () => {
+        console.log('Complete!');
+        console.log(`Time total:   ${Date.now() - startMs}`);
+        console.log(`Time average: ${queue.getStats().average}`);
+    });
 });
 
+const address = process.env.PARALLEL_ADDRESS || '0.0.0.0';
+const port = parseInt(process.env.PARALLEL_PORT, 10) || 9000;
 server.listen(port, address, () => {
     console.info(`[i] Server is up: ${address}:${port}`);
     console.info(`[i] Execute command: ${process.argv}`);
---
+++ parallels/queueWorker.js
@@ -0,0 +1,26 @@
+const Queue = require('better-queue');
+const Task = require('./Task');
+const STATE = require('./state');
+
+
+module.exports = function createQueueWorker(clients) {
+    // eslint-disable-next-line prefer-arrow-callback
+    const q = new Queue(function worker(chunk, cb) {
+        const client = clients.find(c => !c.$busy && (!c.$errors || c.$errors < 2));
+        client.$busy = true;
+
+        const task = new Task(chunk.module.name, chunk.chunk, client);
+        task.once(STATE.FINISHED, (state) => {
+            const success = state === STATE.SUCCESS;
+
+            client.$errors = success ? 0 : (client.$errors || 0) + 1;
+            client.$busy = false;
+
+            cb(success ? null : new Error('Task failed'), task.result);
+        });
+
+        task.$start();
+    }, { concurrent: clients.length });
+
+    return q;
+};
